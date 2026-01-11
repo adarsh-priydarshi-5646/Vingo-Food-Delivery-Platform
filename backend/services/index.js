9dd104b146f4366172fea3f5195c58fbaa8cb303
adarsh-priydarshi-5646 <adarshpriydarshi5646@gmail.com>
2026-01-11 18:54:24 +0530

    docs: format JSDoc comments in index files

--- backend/services/index.js
+++ backend/services/index.js
@@ -1,6 +1,5 @@
 /**
  * Services Index - Export all services from single entry point
  */
-
 export * from './auth.service.js';
 export * from './delivery.service.js';
--- backend/validators/index.js
+++ backend/validators/index.js
@@ -1,7 +1,6 @@
 /**
  * Validators Index - Export all validators from single entry point
  */
-
 export * from './auth.validator.js';
 export * from './order.validator.js';
 export * from './shop.validator.js';
